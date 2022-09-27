c7f7c8d1045b932352b08449a8091fcc76011d1f
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-09-27 16:27:06 +0700

    Done step 3-4 Authentication and Posts section

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,14 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    @posts = Post.all
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-    
+
   end
 end
